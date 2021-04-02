fe2676163eafb5cb67f51367138b53ed400406bb
neeraj15022001 <gneeraj32595@gmail.com>
2021-04-02 17:04:25 +0530

    remove from cart working

--- js/cart.js
+++ js/cart.js
@@ -1,26 +1,60 @@
 let products = getItemFromLocalStorage("products");
 let productsObject = JSON.parse(products);
-let productNameArray = Object.keys(productsObject);
-let productDetailsArray = Object.values(productsObject)
-let tableRefernceHTML = $("#cart-table")
-for(let i = 0; i < productNameArray.length; i++) {
-    let element = `
-    <tr>
-        <td>${productNameArray[i]}</td>
-        <td>${productDetailsArray[i]["price"]}</td>
-        <td>${productDetailsArray[i]["quantity"]}</td>
-    </tr>
-    `
-    tableRefernceHTML.append(element);
-}
-productNameArray.forEach((product) => {
-    console.log(product);
-})
-productDetailsArray.forEach((details) => {
-    console.log(details);
-})
-console.log(productNameArray.length)
+fillTable();
+addEventListener();
+// productNameArray.forEach((product) => {
+//   console.log(product);
+// });
+// productDetailsArray.forEach((details) => {
+//   console.log(details);
+// });
+// console.log(productNameArray.length);
 
 function getItemFromLocalStorage(key) {
-    return localStorage.getItem(key);
+  return localStorage.getItem(key);
+}
+function setItemInLocalStorage(key, value) {
+  localStorage.setItem(key, value);
+}
+
+function fillTable() {
+  let productNameArray = Object.keys(productsObject);
+  let productDetailsArray = Object.values(productsObject);
+//   console.log(productNameArray, productDetailsArray);
+  let tableRefernceHTML = $("#cart-table");
+  for (let i = 0; i < productNameArray.length; i++) {
+    let element = `
+    <tr class="product-details">
+        <td>${productNameArray[i]}</td>
+        <td>${productDetailsArray[i]["price"]}</td>
+        <td class="text-center">${productDetailsArray[i]["quantity"]}</td>
+        <td><button class="bg-danger btn text-light my-2 remove-button" id="${productNameArray[i]}">Remove</button></td>
+    </tr>
+    `;
+    tableRefernceHTML.append(element);
+  }
+}
+
+function addEventListener() {
+    let productNameArray = Object.keys(productsObject);
+    let productDetailsArray = Object.values(productsObject);
+  $(".remove-button").click((event) => {
+    // console.log("remove button clicked");
+    event.preventDefault();
+    //   console.log(productsObject)
+    //   console.log(Object.keys(productsObject).length);
+    //   console.log(event.target.id);
+    //   console.log(event.target.id in productsObject)
+    delete productsObject[event.target.id];
+    setItemInLocalStorage("products", JSON.stringify(productsObject));
+    let indexOfItemRemoved = $.inArray(event.target.id, productNameArray);
+    let totalItemsInCart = parseInt(getItemFromLocalStorage("numberOfItemsCart"));
+    let itemsToBeDeletedFromCart = productDetailsArray[indexOfItemRemoved].quantity;
+    setItemInLocalStorage("numberOfItemsCart", totalItemsInCart - itemsToBeDeletedFromCart)
+    $("tr").remove(".product-details");
+    fillTable();
+    addEventListener();
+    //   console.log(productsObject)
+    //   console.log(Object.keys(productsObject).length);
+  });
 }
--- js/newWomen.js
+++ js/newWomen.js
@@ -1,66 +1,50 @@
-if(getItemFromLocalStorage("numberOfItemsCart") == null) {
-  console.log("value null")
+if (getItemFromLocalStorage("numberOfItemsCart") == null) {
+  console.log("value null");
   var numberOfItemsCart = 0;
   setItemInLocalStorage("numberOfItemsCart", numberOfItemsCart);
-}
-else {
-  var numberOfItemsCart = JSON.parse(getItemFromLocalStorage("numberOfItemsCart"));
+} else {
+  var numberOfItemsCart = JSON.parse(
+    getItemFromLocalStorage("numberOfItemsCart")
+  );
 }
 
-if(getItemFromLocalStorage("products") === null) {
+if (getItemFromLocalStorage("products") === null) {
   var products = {};
-}
-else {
+} else {
   var products = JSON.parse(getItemFromLocalStorage("products"));
 }
 function makingProductObject(productName, productPrice) {
   if (productName in products) {
     products[productName] = {
-      "price" : productPrice,
-      "quantity" : ++products[productName]["quantity"]
+      price: productPrice,
+      quantity: ++products[productName]["quantity"],
     };
   } else {
     products[productName] = {
-      "price" : productPrice,
-      "quantity" : 1
+      price: productPrice,
+      quantity: 1,
     };
   }
 }
 
 $(".add-cart").click((event) => {
   event.preventDefault();
-  console.log("here")
+  console.log("here");
   let parentElement = $(`#${event.target.id}`).parent();
   let productPrice = parentElement.children(".card-text").text();
   let productName = parentElement.children(".card-title").text();
   numberOfItemsCart = incrementItemsInCart(numberOfItemsCart);
-  $("#cart-value").html(`${parseInt(localStorage.getItem("numberOfItemsCart")) + 1}`);
+  $("#cart-value").html(
+    `${parseInt(localStorage.getItem("numberOfItemsCart")) + 1}`
+  );
   makingProductObject(productName, productPrice);
   setItemInLocalStorage("products", JSON.stringify(products));
   setItemInLocalStorage("numberOfItemsCart", JSON.stringify(numberOfItemsCart));
-  // const hasProduct = ifProductExists(productName, productPrice);
-  // if (hasProduct) {
-  //   console.log("true");
-  // } else {
-  //   console.log("false");
-  // }
 });
-
-
 
 function incrementItemsInCart(numberOfItemsCart) {
   return ++numberOfItemsCart;
 }
-
-// function ifProductExists(productName) {
-//   const products = JSON.parse(getItemFromLocalStorage("products"));
-//   const hasProduct = productName in products;
-//   if (hasProduct) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// }
 
 function setItemInLocalStorage(key, value) {
   localStorage.setItem(key, value);
